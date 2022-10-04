f27b47ac05a9133f6831211d92617548e33e0621
Thuy Nguyen <thuicide@gmail.com>
2022-10-03 19:51:08 -0700

    Quiz 4

--- q1.py
+++ q1.py
@@ -1,10 +1,35 @@
+# Quiz: Question 2
+# Programmer: Thuy Nguyen 1989864
+# Description: This program finds the cluster of even numbers
+# within a list of given values
+# Due Date: October 3, 2022
+# Status: Completed
 
-# ******************************
-# Make your Code
-# ******************************
+# Variable for counting even clusters
+counter = 0
 
+# Variables for verifying if even and if ok to continue
+even = False
+okSwitch = False
 
-# Requirement
-# No need to use list
-# All input values are taken one by one separatively.
-###
+# Get 10 values from user
+for i in range(10):
+    num = int(input("Enter an integer: "))
+    
+    # Validate if even and count clusters
+    if num % 2 == 0:
+        if even == True:
+            okSwitch = True
+        else:
+            even = True
+    else:
+        if okSwitch == True:
+            counter += 1
+        okSwitch = False
+        even = False
+
+if okSwitch == True:
+    counter += 1
+
+# Print counter
+print("Number of even clusters:", counter)
